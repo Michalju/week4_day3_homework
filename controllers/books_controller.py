c4a241bc1deadbc9fb8757021a5358b58cc8465a
Michalju <juszkin@gmail.com>
2020-11-25 19:19:32 +0000

    Completed MVP task 1

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint, redirect, request
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
 
+books_blueprint = Blueprint("tasks", __name__)
 # INDEX
 # GET '/books'
+@books_blueprint.route("/books")
+def get_books():
+    books = book_repository.select_all()
+    return render_template("books/index.html", books = books)
 
 # NEW
 # GET '/books/new'
